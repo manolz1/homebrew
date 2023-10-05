bbce471126f36a6ec86947087ed9507f59e6dc16
MaNoLz <47918724+manolz1@users.noreply.github.com>
2023-10-05 13:33:52 +0900

    Update sunshine.rb

--- Formula/sunshine.rb
+++ Formula/sunshine.rb
@@ -4,8 +4,12 @@ class Sunshine < Formula
   desc "Gamestream host/server for Moonlight"
   homepage "https://app.lizardbyte.dev"
   url "https://github.com/LizardByte/Sunshine.git"
+    # is tag required? won't be available until after a release is published
+    #tag:      "v0.20.0",
+    # revision: "11ebb47b3eed5ba397ecea639d137d3804f63d36"
   license all_of: ["GPL-3.0", "BSD-3-Clause", "MIT"]
   head "https://github.com/LizardByte/Sunshine.git", branch: "nightly"
+  version "nightly"
 
   depends_on "boost" => :build
   depends_on "cmake" => :build
@@ -22,7 +26,11 @@ class Sunshine < Formula
       -DCMAKE_BUILD_TYPE=Release
       -DOPENSSL_ROOT_DIR=#{Formula["openssl"].opt_prefix}
       -DSUNSHINE_ASSETS_DIR=sunshine/assets
+      -DCMAKE_CXX_COMPILER=g++
+      -DCMAKE_C_COMPILER=gcc
     ]
+# forced to use gcc bundled with Xcode-beta(which is basically apple clang) 
+# because I'm using a preview version of Sonoma
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
 
     cd "build" do
@@ -30,4 +38,9 @@ class Sunshine < Formula
       system "make", "install"
     end
   end
+
+#  test do
+#    # test that version numbers match
+#    assert_match version.to_s, shell_output("#{bin}/sunshine --version").strip
+#  end
 end
